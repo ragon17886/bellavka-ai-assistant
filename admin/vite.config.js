119f814f1eaaf046490e19488c28c14ca9649b65
Roman Nauras <ragon17886@gmail.com>
2025-11-21 05:42:47 +0300

    Pages

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    sourcemap: true
   },
   server: {
-    port: 3000,
-    host: true
+    port: 3000
   }
 })
